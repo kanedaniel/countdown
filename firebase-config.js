91f814e63d15c5ee1cecbbbecc16aa4fc981c5a1
kanedaniel <hello@kanedaniel.com>
2025-03-14 11:51:56 +1100

    Update firebase-config.js - added firebase registration info

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,15 @@
+// Your web app's Firebase configuration from Step 1
+const firebaseConfig = {
+  apiKey: "AIzaSyCSJBmX_E9h3S-okD5pCaTtpheFHmRPcMk",
+  authDomain: "elf-countdown-e005f.firebaseapp.com",
+  databaseURL: "https://elf-countdown-e005f-default-rtdb.firebaseio.com",
+  projectId: "elf-countdown-e005f",
+  storageBucket: "elf-countdown-e005f.firebasestorage.app",
+  messagingSenderId: "76486311134",
+  appId: "1:76486311134:web:b28563297a3dcc0bbd564f"
+};
 
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
